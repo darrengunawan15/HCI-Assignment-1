6dec3cc9b1fc76029a780dd4590893dbc24d7772
Darren Gunawan <144786206+darrengunawan15@users.noreply.github.com>
2023-10-13 19:34:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let pageNumber = document.getElementsByClassName("page");
 let pageContent1 = document.querySelector(".page-content-1");
 let pageContent2 = document.querySelector(".page-content-2");
+let pageContent3 = document.querySelector(".page-content-3");
 
 pageContent2.style.display = "none";
+pageContent3.style.display = "none";
 
 function activePage(){
     for (p of pageNumber){
@@ -14,8 +16,26 @@ function activePage(){
     if(currentValue == 1){
         pageContent1.style.display = "block";
         pageContent2.style.display = "none";
-    } else{
+        pageContent3.style.display = "none";
+    } else if(currentValue == 2){
         pageContent1.style.display = "none";
         pageContent2.style.display = "block";
+        pageContent3.style.display = "none";
+    } else if(currentValue == 3){
+        pageContent1.style.display = "none";
+        pageContent2.style.display = "none";
+        pageContent3.style.display = "block";
     }
 }
+
+setInterval(()=>{
+    const clockTime = new Date();
+    let hours = document.querySelector(".hrs");
+    let minutes = document.querySelector(".min");
+    let seconds = document.querySelector(".sec");
+
+    hours.innerHTML = (clockTime.getHours()<10?"0":"") + clockTime.getHours();
+    minutes.innerHTML = (clockTime.getMinutes()<10?"0":"") + clockTime.getMinutes();
+    seconds.innerHTML = (clockTime.getSeconds()<10?"0":"") + clockTime.getSeconds();
+},1000)
+
